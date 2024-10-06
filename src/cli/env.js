cfab40e6e8cdb175aff6b60432e97bd66eb01def
Leninbel <leninbel@gmail.com>
2024-10-06 16:15:34 +0200

    parseEnv

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+   const env_obj = process.env;
+   const rss_vars = Object.entries(env_obj).reduce((acc, [key, value]) => {
+
+     if(key.startsWith('RSS_')) {
+        return [...acc, `${key}=${value}` ];
+     }
+
+     return acc;
+   }, []);
+
+   console.log(rss_vars.join(';'));
 };
 
 parseEnv();
