c14f8e978eb3bd6a1fc3ad2b853eb03ad7e61d12
Leninbel <leninbel@gmail.com>
2024-10-06 18:03:06 +0200

    feat: streams write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,32 @@
+import * as fs from 'node:fs';
+import { fileURLToPath } from 'url';
+import { getDirs } from '../utils/getDirs.js';
+import * as path from 'path'
+
+const { filesDir } = getDirs(fileURLToPath(import.meta.url));
+const FILE_NAME = 'fileToWrite.txt'
+const filePath = path.resolve(filesDir, FILE_NAME);
+
+
 const write = async () => {
-    // Write your code here 
+    let data;
+
+    process.stdin.on("readable", () => {
+        data = process.stdin.read();
+        if (data !== null) {
+          process.stdin.emit("end");
+        }
+      });
+      
+    process.stdin.once("end", () => {
+        const stream = fs.createWriteStream(filePath);
+        stream.write(data, 
+            error => {
+              if(error){
+                console.log(error);
+              }
+            });
+      });
 };
 
 await write();
