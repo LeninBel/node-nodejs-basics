a74b02c02439830b32636d2497f943abc035e905
Leninbel <leninbel@gmail.com>
2024-10-07 18:29:14 +0200

    feat: wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,42 @@
+import {Worker} from 'worker_threads';
+import * as os from 'os';
+
+import { fileURLToPath } from 'url';
+import { getDirs } from '../utils/getDirs.js';
+import * as path from 'path'
+
+const { currentDir } = getDirs(fileURLToPath(import.meta.url));
+const workerFile = path.resolve(currentDir, 'worker.js');
+
+
 const performCalculations = async () => {
-    // Write your code here
+const cpus = os.cpus().length;
+const workers = [];
+
+for (let i =0 ; i< cpus; i++) {
+    const pr = new Promise((res, rej)=>{
+        const worker = new Worker(workerFile, {workerData: {num: 10 + i}});
+        worker.on('message', (result) => {
+            res( 
+            {
+                status: 'resolved',
+                data: result
+            });
+        })
+        worker.on("error", () => {
+            res( 
+                {
+                    status: 'error',
+                    data: null
+                });
+         });
+
+    });
+    workers.push(pr);
+}
+
+ Promise.allSettled(workers).then((results) => results.map(res => res.value)).then(console.log);
+
 };
 
 await performCalculations();
